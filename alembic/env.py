0609e0d0368673afee1350af16c66afdcf257cbb
ZifeRRoT <ziferrot@gmail.com>
2022-08-22 14:05:39 +0300

    Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +9,12 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_USER", os.environ.get("DB_USER"))
+config.set_section_option(section, "DB_PASS", os.environ.get("DB_PASS"))
+config.set_section_option(section, "DB_HOST", os.environ.get("DB_HOST"))
+config.set_section_option(section, "DB_NAME", os.environ.get("DB_NAME"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
---
+++ alembic/versions/580547615c64_account_table.py
@@ -0,0 +1,24 @@
+"""account table
+
+Revision ID: 580547615c64
+Revises: 
+Create Date: 2022-08-22 11:15:44.276272
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '580547615c64'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
